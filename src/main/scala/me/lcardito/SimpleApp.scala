06db6fd0a97022307d666fdbb0bb0fc6116874a2
Luigi Cardito <lcardito@gmail.com>
2017-05-03 16:39:45 +0100

    Counting words

--- src/main/scala/me/lcardito/SimpleApp.scala
+++ src/main/scala/me/lcardito/SimpleApp.scala
@@ -5,15 +5,28 @@ import org.apache.spark.{SparkConf, SparkContext}
 object SimpleApp {
 
   def main(args: Array[String]) {
-    val logFile = "YOUR_SPARK_HOME/README.md" // Should be some file on your system
-    val conf = new SparkConf().setAppName("Simple Application").setMaster("local[*]")
+
+    val conf = new SparkConf()
+      .setAppName("Simple Application")
+      .setMaster("local[*]")
+
     val sc = new SparkContext(conf)
+    sc.setLogLevel("ERROR")
+    //TRANSFORMATIONS: no data has been processed yet:
 
+    sc
+      .textFile("file:///usr/local/Cellar/apache-spark/2.1.0/README.md") //Read the file
+      .flatMap(line => line.split(" ")) //Flat map the file to get all the words
+      .map(word => (word, 1)) //Map all the words in a tuple with key the word and value 1
+      .reduceByKey((acc, newValue) => acc + newValue) //Merge the values for each key using an associative and commutative reduce function
+      .sortBy(wordToValue => wordToValue._2, ascending = false) //Sort the tuples by the value on the key (second attribute on the tuple)
+      .saveAsTextFile("output/readMeWordCount") //Save as text file
 
-//    val logData = sc.textFile(logFile, 2).cache()
-//    val numAs = logData.filter(line => line.contains("a")).count()
-//    val numBs = logData.filter(line => line.contains("b")).count()
-//    println(s"Lines with a: $numAs, Lines with b: $numBs")
-//    sc.stop()
+    //Same as count _countByValue but I can't figure out a way to order it
+    //    implicit val ordering = new Ordering[Int] {
+    //      override def compare(x: Int, y: Int): Int = y.compare(x)
+    //    }
+    //    val something = tokenisezData.countByValue()
+
   }
 }
